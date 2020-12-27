4445bb0c9c972dc8494c14550d3a195027b38ee7
kontopro <konteap@gmail.com>
2020-12-27 19:39:24 +0200

    getStatic_Paths
    assid

--- pages/assemblies/[assid].js
+++ pages/assemblies/[assid].js
@@ -1,9 +1,6 @@
 
-const Assembly = (posts) => {
-
-  // const assemblies = master;
-  // const router = useRouter()
-  // const { assid } = router.query
+const Assembly = ({post}) => {
+  
   // const assembly = assemblies.find(({id}) => id === assid)
   // const listItems = assembly.menuItem.length?assembly.menuItem.map(item =>
   //   <div key={item.id} className='card'>
@@ -15,36 +12,41 @@ const Assembly = (posts) => {
 
   return (
     <main>
+      <p>{post.caption}</p>
       {/* <p>Συγκρότημα: {assembly.caption}</p>
       <p>Πλήθος: {assembly.menuItem.length?assembly.menuItem.length:`1`}</p>
       <div className='cards-wrapper'>
       {listItems}
       </div> */}
-      <ul>
-      {posts.map((post) => (
-        <li>{post.caption}</li>
-      ))}
-    </ul>
+      
     </main>
   )
+    }
 
 export const getStaticPaths = async () => {
 
-
-  return {
-    paths:[],
-    fallback:false
-  }
-}}
-
-export const getStaticProps = async () => {
-  
   const res = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/master.json')
   const posts = await res.json()
 
+  const paths = posts.map((post) => ({
+    params: { assid: post.id },
+  }))
+
+  return {
+    paths,
+    fallback:false
+  }
+}
+
+export const getStaticProps = async ({params}) => {
+  
+  const res = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/master.json')
+  const posts = await res.json()
+  const post = posts.find(({id}) => id === params.assid)
+
   return {
     props:{
-      posts
+      post
     }
   }
 }
