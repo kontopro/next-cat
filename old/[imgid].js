d3075ba4c699f5c1de7581b8c8145a4921688141
kontopro <konteap@gmail.com>
2021-01-12 09:22:09 +0200

    Update [imgid].js

--- old/[imgid].js
+++ old/[imgid].js
@@ -1,57 +1,57 @@
-import Link from 'next/link';
-import Listnsn from '../../../components/Listnsn'
-import Submenu from '../../../components/Submenu'
+// import Link from 'next/link';
+// import Listnsn from '../../../components/Listnsn'
+// import Submenu from '../../../components/Submenu'
 
-function Imag({ imag, nsns }) {
+// function Imag({ imag, nsns }) {
 
-  return (
-    <>
-    <Submenu />
-    <main className='imgid'>
-      <p>{imag.name}</p>
-      <p>
-      <img src={`/images-m109/${imag.id}.jpg`} alt="me" width="500" height='auto' />
-      </p>
-      <Listnsn details={nsns} />
-    </main>
-    </>
-);
-}
+//   return (
+//     <>
+//     <Submenu />
+//     <main className='imgid'>
+//       <p>{imag.name}</p>
+//       <p>
+//       <img src={`/images-m109/${imag.id}.jpg`} alt="me" width="500" height='auto' />
+//       </p>
+//       <Listnsn details={nsns} />
+//     </main>
+//     </>
+// );
+// }
 
-export const getStaticPaths = async () => {
+// export const getStaticPaths = async () => {
 
-  const res = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/master-m109.json')
-  const assemblies = await res.json()
-  const paths = assemblies.map((assembly) => assembly.menuItem.length?assembly.menuItem.map((item) => (
-      { 
-          params: { assid: assembly.id, imgid: item.id } 
-    }
-    )
-    ):{params: { assid: assembly.id, imgid: assembly.menuItem.id}}
-).flat()
+//   const res = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/master-m109.json')
+//   const assemblies = await res.json()
+//   const paths = assemblies.map((assembly) => assembly.menuItem.length?assembly.menuItem.map((item) => (
+//       { 
+//           params: { assid: assembly.id, imgid: item.id } 
+//     }
+//     )
+//     ):{params: { assid: assembly.id, imgid: assembly.menuItem.id}}
+// ).flat()
 
-  return {
-    paths,
-    fallback:false
-  }
-}
+//   return {
+//     paths,
+//     fallback:false
+//   }
+// }
 
-export const getStaticProps = async ({params}) => {
+// export const getStaticProps = async ({params}) => {
   
-  const res1 = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/detail-m109.json')
-  const details = await res1.json()
-  const nsns = details.filter(item => item.PictureNo === params.imgid)
-  const res = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/master-m109.json')
-  const assemblies = await res.json()
-  const assembly = assemblies.find(item => item.id === params.assid)
-  const imag = assembly.menuItem.length?assembly.menuItem.find(item => item.id === params.imgid):assembly.menuItem
+//   const res1 = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/detail-m109.json')
+//   const details = await res1.json()
+//   const nsns = details.filter(item => item.PictureNo === params.imgid)
+//   const res = await fetch('https://raw.githubusercontent.com/kontopro/next-cat/main/data/master-m109.json')
+//   const assemblies = await res.json()
+//   const assembly = assemblies.find(item => item.id === params.assid)
+//   const imag = assembly.menuItem.length?assembly.menuItem.find(item => item.id === params.imgid):assembly.menuItem
 
-  return {
-    props:{
-      imag, 
-      nsns
-    }
-  }
-}
+//   return {
+//     props:{
+//       imag, 
+//       nsns
+//     }
+//   }
+// }
 
-export default Imag
+// export default Imag
